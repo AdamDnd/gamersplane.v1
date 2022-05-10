b7764360b0367d4d805b06ddc8f517d2bd344506
Adam <adamdnd@devprocess.com>
2022-05-10 06:37:40 +0100

    Resize editor on mobile

--- javascript/markItUp/jquery.markitup.js
+++ javascript/markItUp/jquery.markitup.js
@@ -159,17 +159,17 @@
 				if (options.resizeHandle === true && browser.safari !== true) {
 					resizeHandle = $('<div class="markItUpResizeHandle"></div>')
 						.insertAfter($$)
-						.bind("mousedown.markItUp", function(e) {
-							var h = $$.height(), y = e.clientY, mouseMove, mouseUp;
+						.bind("mousedown touchstart", function(e) {
+							var h = $$.height(), y = (e.clientY?e.clientY:e.originalEvent.touches[0].clientY), mouseMove, mouseUp;
 							mouseMove = function(e) {
-								$$.css("height", Math.max(20, e.clientY+h-y)+"px");
+								$$.css("height", Math.max(20, (e.clientY?e.clientY:e.originalEvent.touches[0].clientY)+h-y)+"px");
 								return false;
 							};
 							mouseUp = function(e) {
-								$("html").unbind("mousemove.markItUp", mouseMove).unbind("mouseup.markItUp", mouseUp);
+								$("html").unbind("mousemove.markItUp touchmove.markItUp", mouseMove).unbind("mouseup.markItUp touchend.markItUp", mouseUp);
 								return false;
 							};
-							$("html").bind("mousemove.markItUp", mouseMove).bind("mouseup.markItUp", mouseUp);
+							$("html").bind("mousemove.markItUp touchmove.markItUp", mouseMove).bind("mouseup.markItUp touchend.markItUp", mouseUp);
 					});
 					footer.append(resizeHandle);
 				}
